6cc33393956bd946ffcf07b8a99536edd3d3a33f
MusaZz <musa.agayev.21@gmail.com>
2024-01-09 10:32:44 +0400

    Initial Commit

---
+++ Apple-Frameworks-List/Apple_Frameworks_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_ListApp.swift
+//  Apple-Frameworks-List
+//
+//  Created by macbook on 09.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
